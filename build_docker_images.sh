d4dfd3795f3bf1fddc760918e38160eaef008d0e
Tamas Beri <tamas.beri@gmail.com>
2025-03-25 19:46:00 +0100

    docker changes

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pwd=$(pwd)
-docker build -t cmaketemplate:base $pwd/docker/base
-docker build -t cmaketemplate:builder --secret id=auth_conf,src=/etc/apt/auth.conf.d/mini_dinstall.conf $pwd/docker/builder
-docker build -t cmaketemplate:builder $pwd/docker/runtime
+image=cmaketemplate
+docker build -t $image:base $pwd/docker/base
+docker build -t $image:builder $pwd/docker/builder
+docker build -t $image:runtime $pwd/docker/runtime
