7898ea109b7ca9de4b63b3d9888bbe21a3e8d55e
Tamas Beri <tamas.beri@kuka.com>
2025-03-26 07:53:42 +0100

    added compiler flags

--- build_project.sh
+++ build_project.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-docker run -it --rm --entrypoint sh -v $(pwd):/work -w /work cmaketemplate:builder -c "mkdir -p /work/build && cd /work/build && cmake .. && make"
+
+cmake_build_type=$1
+
+if [ -z "$cmake_build_type" ]; then
+ cmake_build_type=Debug
+ echo "Variable cmake_build_type not defined, using default: $cmake_build_type"
+else
+ echo "Using variable cmake_build_type: $cmake_build_type"
+fi
+
+docker run -it --rm --entrypoint sh -v $(pwd):/work -w /work cmaketemplate:builder -c "mkdir -p /work/build/$cmake_build_type && cd /work/build/$cmake_build_type && cmake -DCMAKE_BUILD_TYPE=$cmake_build_type ../.. && make"
