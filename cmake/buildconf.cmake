d4959dea3cb708b2fc05f3f7c14835d20edeb6cd
Tamas Beri <tamas.beri@kuka.com>
2025-03-26 13:33:38 +0100

    separated build config cmake

---
+++ cmake/buildconf.cmake
@@ -0,0 +1,12 @@
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++${CMAKE_CXX_STANDARD} -Wall -Wextra -Werror -Wpedantic")
+
+if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
+ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ftest-coverage -fprofile-arcs -g -O0")
+elseif("${CMAKE_BUILD_TYPE}" STREQUAL "Release")
+ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3")
+elseif("${CMAKE_BUILD_TYPE}" STREQUAL "RelWithDebInfo")
+ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -O3")
+else()
+ message(FATAL_ERROR "Unsupported CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE}")
+endif()
