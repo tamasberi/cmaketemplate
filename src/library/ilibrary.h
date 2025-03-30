6c32fbc913e6404398b058d5080c311a71cf14e5
Tamas Beri <tamas.beri@gmail.com>
2025-03-30 12:50:15 +0200

    added comments to get rid of doxygen warnings

--- src/library/ilibrary.h
+++ src/library/ilibrary.h
@@ -1,10 +1,18 @@
 #pragma once
 
+//! \file ilibrary.h
+//! \brief The header file for the ILibrary class.
+
 #include <string>
 
+//! \brief The abstract ILibrary interface class.
 class ILibrary
 {
 public:
+   //! \brief Destructor, intentionally left unimplemented.
    virtual ~ILibrary() = default;
+   //! \brief A dummy function does nothing.
+   //! \param somePar A string parameter.
+   //! \return The return value,
    virtual bool doSomething(const std::string& somePar) = 0;
 };
--- src/library/library.h
+++ src/library/library.h
@@ -1,7 +1,11 @@
 #pragma once
 
+//! \file library.h
+//! \brief The header file for the Library class.
+
 #include "ilibrary.h"
 
+//! \brief An implementation of the ILibrary interface.
 class Library : public ILibrary
 {
 public:
